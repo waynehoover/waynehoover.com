6fb5a2d3337b41b959c5dd752b7d480c166ba7e2
Wayne Hoover <w@waynehoover.com>
2020-05-11 14:47:45 -1000

    use highway.js and quicklinks

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+import Highway from '@dogstudio/highway';
+import Quicklink from 'quicklink/dist/quicklink.mjs';
+
+const H = new Highway.Core();
+
+H.on('NAVIGATE_END', ({ to }) => {
+  // Call Quicklink
+  Quicklink({
+    el: to.view
+  });
+});
